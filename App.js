9c233d987228575accd704a6ed6a3ecb5a2144dd
roluochKe <rasholuoch@gmail.com>
2021-09-03 15:37:11 +0300

    adding carlist component

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
-import CarItem from './components/CarItem'
+// import CarItem from './components/CarItem'
+import CarsList from './components/CarsList'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem
+      {/* <CarItem
         name={'Model X'}
         tagline={'Order Online Now'}
         taglineCTA={'Touchless Delivery'}
         image={require('./assets/images/ModelX.jpeg')}
-      />
+      /> */}
+      <CarsList />
       <StatusBar style='auto' />
     </View>
   )
