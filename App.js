b3ec8c5c8e6a6bbb07bada2678640d8446dabcf7
roluochKe <rasholuoch@gmail.com>
2021-09-03 15:54:04 +0300

    adding header component

--- App.js
+++ App.js
@@ -1,18 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
-// import CarItem from './components/CarItem'
+import Header from './components/Header'
 import CarsList from './components/CarsList'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <CarItem
-        name={'Model X'}
-        tagline={'Order Online Now'}
-        taglineCTA={'Touchless Delivery'}
-        image={require('./assets/images/ModelX.jpeg')}
-      /> */}
+      <Header />
       <CarsList />
       <StatusBar style='auto' />
     </View>
