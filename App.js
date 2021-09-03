9a182898bb34b0038d3d6ef7bf21afc944d8df25
roluochKe <rasholuoch@gmail.com>
2021-09-03 12:44:37 +0300

    project installation

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.carContainer}>
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text styles={styles.subtitle}>Starting at $69,000</Text>
+        </View>
+      </View>
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +23,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+
+  carContainer: {},
+
+  titles: {},
+
+  title: {},
+
+  subtitle: {},
+})
